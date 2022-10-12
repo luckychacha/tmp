d832fdc1a1327eb351ef12557fce92c6552cd5f5
luckychacha <358140284@qq.com>
2022-10-12 23:34:35 +0800

    format mod name.

--- src/leetcode/mod.rs
+++ src/leetcode/mod.rs
@@ -1,9 +1,9 @@
-mod decode_string;
-mod can_make_pali_queries;
-mod find_maximum_xor;
-mod is_ideal_permutation;
-mod maximum_binary_string;
-mod merge_in_between;
-mod interpret;
+mod lt_00067_jz_find_maximum_xor;
+mod lt_01678_interpret;
+mod lt_00394_decode_string;
+mod lt_00775_is_ideal_permutation;
+mod lt_01177_can_make_pali_queries;
+mod lt_01669_merge_in_between;
+mod lt_01702_maximum_binary_string;
 
 pub struct Solution();
