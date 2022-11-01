56095a57a805701611ad8ed42379607f0ef16044
luckychacha <358140284@qq.com>
2022-11-01 23:59:50 +0800

    leetcode day 22: edit

--- src/leetcode/lt_00915_partition_disjoint.rs
+++ src/leetcode/lt_00915_partition_disjoint.rs
@@ -2,42 +2,24 @@ use super::Solution;
 
 impl Solution {
     pub fn partition_disjoint(nums: Vec<i32>) -> i32 {
-        let mut i: usize = 0;
-        let mut j: usize = nums.len() - 1;
-        let mut left_max = nums[0];
+        let mut prev_max = nums[0];
+        let mut total_max = nums[0];
 
-        while i != j {
-            if nums[i] <= left_max {
-                i += 1;
-                continue;
-            }
-            if nums[j] >= left_max {
-                j -= 1;
-            } else {
-                // println!("{i} {j} {left_max}");
+        let mut answer = 0;
 
-                i += 1;
-                left_max = left_max.max(nums[i]);
-                j = nums.len() - 1;
+        for i in 1..nums.len() {
+            total_max = total_max.max(nums[i]);
+
+            if nums[i] < prev_max {
+                prev_max = total_max;
+                answer = i;
             }
+
         }
 
-
-        let a = (1..nums.len()).fold(
-            (1, nums[0], nums[0]),
-            |(partition, prev_max, curr_max), i| {
-                if nums[i] < prev_max { 
-                    (i as i32 + 1, curr_max, curr_max)
-                } else {
-                    (partition, prev_max, curr_max.max(nums[i]))
-                }
-            }
-        ).0;
-        i as i32 + 1
+        answer as i32 + 1
     }
 }
-
-
 #[cfg(test)]
 mod test {
     use super::*;
@@ -46,7 +28,11 @@ mod test {
     fn partition_disjoint_should_work() {
         assert_eq!(
             Solution::partition_disjoint(vec![1,1,1,0,6,12]),
-            7
+            4
+        );
+        assert_eq!(
+            Solution::partition_disjoint(vec![1,1]),
+            1
         );
     }
 
