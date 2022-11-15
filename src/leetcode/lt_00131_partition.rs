a61555a69988772d0bf1a8594ef8c4376ffff482
luckychacha <358140284@qq.com>
2022-11-15 18:50:44 +0800

    leetcode day 24: new.

---
+++ src/leetcode/lt_00131_partition.rs
@@ -0,0 +1,45 @@
+use super::Solution;
+
+impl Solution {
+    pub fn partition(s: String) -> Vec<Vec<String>> {
+        let mut res: Vec<Vec<String>> = vec![];
+        Solution::backstrace(&s.chars().collect(), 0, &mut res, &mut vec![]);
+        res
+    }
+
+    fn backstrace(
+        s: &Vec<char>,
+        start_index: usize,
+        res: &mut Vec<Vec<String>>,
+        path: &mut Vec<String>,
+    ) {
+        if start_index >= s.len() {
+            res.push(path.clone());
+            return;
+        }
+        for i in start_index..s.len() {
+            match Solution::checked(start_index, i, s) {
+                true => {
+                    path.push(s[start_index..=i].into_iter().collect());
+                    Solution::backstrace(s, i + 1, res, path);
+                    path.pop();
+                }
+                _ => continue,
+            }
+        }
+    }
+
+    fn checked(start: usize, end: usize, s: &Vec<char>) -> bool {
+        let mut start = start;
+        let mut end = end;
+        while start < end {
+            if s.get(start) != s.get(end) {
+                return false;
+            }
+            start += 1;
+            end -= 1;
+        }
+
+        true
+    }
+}
